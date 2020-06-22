bcc7a12584304f8ab0ef69bf1a6cde6cb9474731
Mohammed <itssmohammed@gmail.com>
2020-06-22 23:24:38 +0300

    updates to bgimage

--- js/script.js
+++ js/script.js
@@ -2,18 +2,46 @@
 let landingPage = document.querySelector(".landingPage");
 
 // images array
-
 let imgsArray  = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 
-setInterval(() => {
+let index = 0;
+let iterations = 0;
 
-  // get random number 
+// Change bg image every 15 seconds 
+const updateBgImage = function() {
+  
+  /* reset index to zero if current index is greater than number of images.
+  increment iterations variable since it means we've done one whole loop.
+   */
+  if (index >= imgsArray.length) {
+    index = 0;
+    iterations++;
+  }
 
-  let randomNumber = Math.floor(Math.random() * imgsArray.length);
-  // Change background img url
+  // set the background image
+  landingPage.style.backgroundImage = 'url("images/' + imgsArray[index] +'")';
 
-landingPage.style.backgroundImage = 'url("images/' + imgsArray[randomNumber] +'")';
-}, 15000);
+  /* clear interval (stop timer) when we've reached
+   * the number of iterations allowed.
+   */
+  if (iterations >= 3) {
+    iterations = 0;
+  } else {
+    /* increment current index in order to get next image
+     * when this function gets called again.
+     */
+    index++;
+  }
+}
+
+// update first image
+updateBgImage();
+
+// initiate timer
+const interval = setInterval(updateBgImage, 10000);
+
+
+
 
 // Slide Menu
 
